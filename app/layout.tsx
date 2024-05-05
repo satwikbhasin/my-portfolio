3bbeaf88a37d469cb0e424de383aa036909cb290
satwikbhasin <satwikbhasin@gmail.com>
2024-05-04 21:32:00 -0400

    analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,13 +59,13 @@ export default function RootLayout({
   return (
     <html lang="en" className={[inter.variable, calSans.variable].join(" ")}>
       <head>
-        <Analytics />
       </head>
       <body
         className={`bg-black ${process.env.NODE_ENV === "development" ? "debug-screens" : undefined
           }`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
