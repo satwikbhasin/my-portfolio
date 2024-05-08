fd163cc61092a09a4ab4bd65425498a172e3131e
satwikbhasin <satwikbhasin@gmail.com>
2024-05-08 17:39:05 -0400

    font addition

--- app/projects/[slug]/header.tsx
+++ app/projects/[slug]/header.tsx
@@ -109,7 +109,7 @@ export const Header: React.FC<Props> = ({ project, views }) => {
 						<div className="grid grid-cols-1 gap-y-6 gap-x-8 text-base font-semibold leading-7 text-white sm:grid-cols-2 md:flex lg:gap-x-10">
 							{links.map((link) => (
 								<Link target="_blank" key={link.label} href={link.href}>
-									<div className="text-sea-green hover:text-zinc-100">
+									<div className="text-sea-green hover:text-zinc-100 font-kode-mono">
 										{link.label} <span aria-hidden="true">&rarr;</span>
 									</div>
 								</Link>
