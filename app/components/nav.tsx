a96bd0b6c6cf284197cb2b95f72d4caa5bdb15c8
satwikbhasin <satwikbhasin@gmail.com>
2024-05-11 10:01:28 -0400

    backdrop-blur-forced

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -22,9 +22,9 @@ export const Navigation: React.FC = () => {
 	return (
 		<header ref={ref}>
 			<div
-				className={`fixed inset-x-0 top-0 z-50 backdrop-blur  duration-200 border-b  ${isIntersecting
+				className={`fixed inset-x-0 top-0 z-50 backdrop-blur duration-200 border-b  ${isIntersecting
 					? "bg-zinc-900/0 border-transparent"
-					: "bg-zinc-900/500  border-zinc-800 "
+					: "bg-zinc-900/500 border-zinc-800 "
 					}`}
 			>
 				<div className="container flex flex-row-reverse items-center justify-between p-6 mx-auto">
@@ -32,7 +32,7 @@ export const Navigation: React.FC = () => {
 						<Link
 							href="/projects"
 							className={`duration-200 ${pathName === '/projects' ? 'text-sea-green' : 'text-zinc-400 hover:text-zinc-100'} font-kode-mono`}>
-								projects
+							projects
 						</Link>
 						<Link
 							href="/resume"
