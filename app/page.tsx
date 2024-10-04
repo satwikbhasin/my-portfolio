56c58cdbd1d9589844e99e290f753ae5d3095b79
Satwik Bhasin <satwikbhasin@gmail.com>
2024-10-04 17:58:01 -0400

    name added to env

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
         quantity={200}
       />
       <h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-        Satwik Bhasin
+        {process.env.NEXT_PUBLIC_MY_NAME}
       </h1>
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-5 text-center animate-fade-in">
